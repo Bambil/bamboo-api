45969c19679eeef3f22da2cfe27bd966835b0cb7
Parham Alvani <parham.alvani@gmail.com>
2017-06-19 15:46:56 +0430

    I1820 thing was added.

---
+++ i1820/thing.js
@@ -0,0 +1,20 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 19-06-2017
+ * |
+ * | File Name:     thing.js
+ * +===============================================
+ */
+
+
+class I1820Thing {
+  constructor(id, agentId, type) {
+    this.id = id;
+    this.agentId = agentId;
+    this.type = type;
+  }
+}
+
+module.exports = I1820Thing;
