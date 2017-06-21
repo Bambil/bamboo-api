ca8f24ab1ec8d0e63d2b885b4ccb324c9f086922
Parham Alvani <parham.alvani@gmail.com>
2017-06-21 09:55:06 +0430

    Let's move thing log into things :)

--- i1820/thing.js
+++ i1820/thing.js
@@ -10,10 +10,25 @@
 
 
 class I1820Thing {
-  constructor(id, agentId, type) {
+  constructor(client, id, agentId, type) {
+    this.client = client;
     this.id = id;
     this.agentId = agentId;
     this.type = type;
+  }
+
+  get log() {
+    return new Promise((resolve, reject) => {
+      this.client.post('/thing', {
+        'type': this.type,
+        'agent_id': this.agentId,
+        'device_id': this.id,
+        'states': []
+      }).then((response) => {
+        const result = response.data;
+        return resolve(result);
+      }).catch((err) => reject(err));
+    });
   }
 }
 
