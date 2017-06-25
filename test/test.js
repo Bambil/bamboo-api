c05620c14061e3632012de96922a8494e5a388f9
Parham Alvani <parham.alvani@gmail.com>
2017-06-25 20:27:26 +0430

    Test module was improved.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,19 @@
 const I1820Client = require('..').I1820Client;
+const tape = require('tape');
 
-const i1820 = new I1820Client('http://iot.ceit.aut.ac.ir:58902');
+tape('Data retrieve with Aolab', (t) => {
+  t.plan(5);
 
-i1820.discovery().then((agents) => {
-  agents.forEach((agent) => {
-    agent.things.forEach((thing) => {
-      if (thing.type === 'multisensor') {
-        i1820.getLog(thing).then((result) => {
-          console.log(result);
+  const i1820 = new I1820Client('http://iot.ceit.aut.ac.ir:58902');
+
+  i1820.discovery().then((agents) => {
+    agents.forEach((agent) => {
+      agent.getThingsByType('multisensor').forEach((thing) => {
+        thing.log.then((result) => {
+          t.comment(JSON.stringify(result));
+          t.equal(typeof result, 'object');
         });
-      }
+      });
     });
   });
 });
