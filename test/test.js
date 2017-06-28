9557f3b377f1cb11e427c36b08f1c435115f8834
Parham Alvani <parham.alvani@gmail.com>
2017-06-28 12:31:18 +0430

    Test module was improved.

--- test/test.js
+++ test/test.js
@@ -1,19 +1,50 @@
-const I1820Client = require('..').I1820Client;
-const tape = require('tape');
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 28-06-2017
+ * |
+ * | File Name:     test.js
+ * +===============================================
+ */
+/* eslint-env mocha */
 
-tape('Data retrieve with Aolab', (t) => {
-  t.plan(5);
+const I1820Client = require('..').I1820Client
+const assert = require('assert')
 
-  const i1820 = new I1820Client('http://iot.ceit.aut.ac.ir:58902');
+describe('Aolab', function () {
+  this.timeout(3000)
 
-  i1820.discovery().then((agents) => {
-    agents.forEach((agent) => {
-      agent.getThingsByType('multisensor').forEach((thing) => {
-        thing.log.then((result) => {
-          t.comment(JSON.stringify(result));
-          t.equal(typeof result, 'object');
-        });
-      });
-    });
-  });
-});
+  const i1820 = new I1820Client('http://iot.ceit.aut.ac.ir:58902')
+  let i1820Agents = []
+
+  before('discovery', (done) => {
+    i1820.discovery().then((agents) => {
+      assert.ok(Array.isArray(agents))
+      i1820Agents = agents
+      done()
+    })
+  })
+
+  describe('log', () => {
+    it('mutlisensor', (done) => {
+      i1820Agents.forEach((agent) => {
+        agent.getThingsByType('multisensor')[0].log.then((result) => {
+          console.log(JSON.stringify(result['light']))
+          assert.equal(typeof result, 'object')
+          done()
+        })
+      })
+    })
+
+    it('gas', (done) => {
+      i1820Agents.forEach((agent) => {
+        agent.getThingsByType('gas')[0].log.then((result) => {
+          console.log(JSON.stringify(result['gas']))
+          assert.equal(typeof result, 'object')
+          done()
+        })
+      })
+    })
+  })
+})
