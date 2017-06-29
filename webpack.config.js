d92e996b4cecc53ebce4ea8a9a8135a5dc3fa422
Parham Alvani <parham.alvani@gmail.com>
2017-06-30 00:35:05 +0430

    Webpack configuration was added.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require('webpack')
+
+function generateConfig (name) {
+  var uglify = name.indexOf('min') > -1
+  var config = {
+    entry: './index.js',
+    output: {
+      filename: './dist/' + name + '.js',
+      sourceMapFilename: './dist/' + name + '.map',
+      library: 'i1820',
+      libraryTarget: 'umd'
+    },
+    node: {
+      process: false
+    },
+    devtool: 'source-map'
+  }
+
+  config.plugins = [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+
+  if (uglify) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compressor: {
+          warnings: false
+        }
+      })
+    )
+  }
+
+  return config
+}
+
+module.exports = generateConfig('i1820')
