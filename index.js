7320b53420745b37be5461a2cef38c34ebfe9b98
Parham Alvani <parham.alvani@gmail.com>
2017-07-03 02:20:30 +0430

    Expose all inner things.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const I1820Client = require('./i1820/client');
+const I1820Client = require('./i1820/client')
+const I1820Thing = require('./i1820/thing')
+const I1820Agent = require('./i1820/agent')
 
 module.exports = {
-  I1820Client
-};
+  I1820Client,
+  I1820Thing,
+  I1820Agent
+}
